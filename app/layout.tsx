1ac61938f6a65d2ca206bfc2196901de828ff011
Ornella11 <124208143+Ornella11@users.noreply.github.com>
2025-03-12 20:45:25 +0100

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${montserrat.className} tracking-tight`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
