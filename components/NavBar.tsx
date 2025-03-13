b78325e6ab87cad01d1b6b82a8af478f71e623d6
Ornella11 <124208143+Ornella11@users.noreply.github.com>
2025-03-13 22:28:30 +0100

    Update

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -29,7 +29,6 @@ const Navbar = () => {
       {/* Navbar */}
       <div className="relative flex w-full justify-between items-center">
         {/* Logo à gauche */}
-        {/* <div className="text-yellow font-semibold text-lg z-10" >Ornella Ormb .</div> */}
         <div className="z-10">
           <Link href="/" className="text-yellow font-semibold text-lg">
             Ornella Ormb .
@@ -57,7 +56,7 @@ const Navbar = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="none"
-                stroke="currentColor" // Permet de changer la couleur via Tailwind
+                stroke="currentColor"
                 strokeWidth="2"
                 strokeLinecap="round"
                 className="w-8 h-8 text-yellow hover:text-[#FFFDA3] transition-colors duration-300"
